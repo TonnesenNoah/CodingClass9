46e9c05e092efb63c26d6418b606b289e3bb21eb
TonnesenNoah <88952739+TonnesenNoah@users.noreply.github.com>
2021-08-14 18:03:49 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var sprite;
+
+function setup() {
+  createCanvas(400,400);
+
+  sprite = createSprite(175, 175)
+  sprite.shapeColor = rgb(180,50,230)
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+
+}
+
+
+
+
